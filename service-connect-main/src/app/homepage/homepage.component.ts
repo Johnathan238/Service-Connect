f03263986500d03ca5c3da957af8df01592fd1f2
Johnathan <jonathanumana86@gmail.com>
2023-04-22 21:10:08 -0400

    Added more cards using ngFor & just styling for now

--- service-connect-main/src/app/homepage/homepage.component.ts
+++ service-connect-main/src/app/homepage/homepage.component.ts
@@ -8,4 +8,43 @@ import {MatCardModule} from '@angular/material/card';
 })
 export class HomepageComponent {
 
+  cards = [
+    {
+      title: "Dog 1",
+      Name: "Shiba",
+      Breed: "Dog Breed",
+      img: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      description: "The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. \
+      A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally \
+      bred for hunting."
+    },
+    {
+      title: "Dog 2",
+      Name: "Shiba",
+      Breed: "Dog Breed",
+      img: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      description: "The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. \
+      A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally \
+      bred for hunting."
+    },
+    {
+      title: "Dog 3",
+      Name: "Shiba",
+      Breed: "Dog Breed",
+      img: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      description: "The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. \
+      A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally \
+      bred for hunting."
+    },
+    {
+      title: "Dog 4",
+      Name: "Shiba",
+      Breed: "Dog Breed",
+      img: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      description: "The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. \
+      A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally \
+      bred for hunting."
+    }
+  ]
+
 }
